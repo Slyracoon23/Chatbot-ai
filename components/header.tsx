2862944368bbafe8124888bc81b72b085ef46e14
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-23 01:30:27 +0200

    walletconnect

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import Web3ConnectButton from './web3connect-button'
 import { ThemeToggle } from './theme-toggle'
 
 export async function Header() {
+  // const session = await auth()
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-muted/50 px-4">
       <div className="flex w-full items-stretch justify-end gap-2">
