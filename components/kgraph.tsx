f0fcc5c3a83c4c68cf87beb2215569073b44ef1c
Earl Potters <earl.potters@gmail.com>
2023-07-22 13:38:52 +0200

    add neo4j digital mirror

--- components/kgraph.tsx
+++ components/kgraph.tsx
@@ -1,46 +1,62 @@
-'use client';
-import React, { useState } from "react";
-import { GraphCanvas } from "reagraph";
+'use client'
+import React, { useState, useEffect } from 'react'
+import { GraphCanvas } from 'reagraph'
+import { useReadCypher } from 'use-neo4j'
 
 export const KGraph = () => {
-  const [nodes, setNodes] = useState([
-    {
-      id: "1",
-      label: "1",
-    },
-    {
-      id: "2",
-      label: "2",
-    },
-  ]);
+  const [nodes, setNodes] = useState([])
+  const [edges, setEdges] = useState([])
 
-  const [edges, setEdges] = useState([
-    {
-      source: "1",
-      target: "2",
-      id: "1-2",
-      label: "1-2",
-    },
-    {
-      source: "2",
-      target: "1",
-      id: "2-1",
-      label: "2-1",
-    },
-  ]);
+  const {
+    loading: nodesLoading,
+    records: nodesRecords,
+    run: runNodesQuery
+  } = useReadCypher('MATCH (n) RETURN n LIMIT 10')
 
-  const addNode = () => {
-    const newNode = {
-      id: (nodes.length + 1).toString(),
-      label: (nodes.length + 1).toString(),
-    };
-    setNodes([...nodes, newNode]);
-  };
+  const {
+    loading: edgesLoading,
+    records: edgesRecords,
+    run: runEdgesQuery
+  } = useReadCypher('MATCH ()-[r]->() RETURN r LIMIT 10')
+
+  useEffect(() => {
+    // Run the queries when the component mounts
+    runNodesQuery()
+    runEdgesQuery()
+  }, [])
+
+  useEffect(() => {
+    // Update the nodes state when the nodesRecords changes
+    if (nodesRecords) {
+      const newNodes = nodesRecords.map(record => {
+        const node = record.get('n')
+        return { id: node.identity.toString(), label: node.labels[0] }
+      })
+      setNodes(newNodes)
+    }
+  }, [nodesRecords])
+
+  useEffect(() => {
+    // Update the edges state when the edgesRecords changes
+    if (edgesRecords) {
+      const newEdges = edgesRecords.map(record => {
+        const relationship = record.get('r')
+        return {
+          source: relationship.start.toString(),
+          target: relationship.end.toString(),
+          id: relationship.identity.toString(),
+          label: relationship.type
+        }
+      })
+      setEdges(newEdges)
+    }
+  }, [edgesRecords])
+
+  if (nodesLoading || edgesLoading) return <div>Loading...</div>
 
   return (
     <div>
-
       <GraphCanvas nodes={nodes} edges={edges} />
     </div>
-  );
-};
+  )
+}
