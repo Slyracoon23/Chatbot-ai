6b898a8a7f19d4018dbeb3aecc5b3606eacb6b70
Earl Potters <earl.potters@gmail.com>
2023-07-22 21:10:25 +0200

    checkpoint

--- app/eas/page.tsx
+++ app/eas/page.tsx
@@ -1,29 +1,70 @@
-import { SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
-import { ethers } from "ethers";
+'use client'
+import React, { useEffect, useState } from 'react'
 
-const offchain = await eas.getOffchain();
+import {
+  EAS,
+  Offchain,
+  SchemaEncoder,
+  SchemaRegistry
+} from '@ethereum-attestation-service/eas-sdk'
+import { ethers } from 'ethers'
 
-// Initialize SchemaEncoder with the schema string
-const schemaEncoder = new SchemaEncoder("uint256 eventId, uint8 voteIndex");
-const encodedData = schemaEncoder.encodeData([
-  { name: "eventId", value: 1, type: "uint256" },
-  { name: "voteIndex", value: 1, type: "uint8" },
-]);
+const EASContractAddress = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e' // Sepolia v0.26
 
-const privateKey = '78f847335d13b4ddf6e2e279515f48d2246256bd910b4f007fa3e6ac16e7887a';
-// Signer is an ethers.js Signer instance
-const provider = ethers.getDefaultProvider();  // or use a specific provider like ethers.providers.InfuraProvider
-const signer = new ethers.Wallet(privateKey, provider);
+const AttestationComponent = () => {
+  const [isValidSignature, setIsValidSignature] = useState(false)
 
-const offchainAttestation = await offchain.signOffchainAttestation({
-  recipient: '0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165',
-  // Unix timestamp of when attestation expires. (0 for no expiration)
-  expirationTime: 0,
-  // Unix timestamp of current time
-  time: 1671219636,
-  revocable: true,
-  nonce: 0,
-  schema: "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995",
-  refUID: '0x0000000000000000000000000000000000000000000000000000000000000000',
-  data: encodedData,
-}, signer);
+  useEffect(() => {
+    const createAttestation = async () => {
+      // Assume that eas and sender are initialized elsewhere in your app
+      const eas = new EAS(EASContractAddress)
+      const provider = ethers.providers.getDefaultProvider('sepolia')
+
+      const offchain = await eas.getOffchain()
+
+      const attestationData = {
+        recipient: signer.address,
+        // Unix timestamp of when attestation expires. (0 for no expiration)
+        expirationTime: 0,
+        // Unix timestamp of current time
+        time: 1671219636,
+        revocable: true,
+        version: 1,
+        nonce: 0,
+        schema:
+          '0xf58b8b212ef75ee8cd7e8d803c37c03e0519890502d5e99ee2412aae1456cafe',
+        refUID:
+          '0x0000000000000000000000000000000000000000000000000000000000000000',
+        data: '0x0000000000000000000000000000000000000000000000000000000000000000'
+      }
+
+      const privateKey =
+        '78f847335d13b4ddf6e2e279515f48d2246256bd910b4f007fa3e6ac16e7887a'
+      const signer = new ethers.Wallet(privateKey, provider)
+
+      const response = await offchain.signOffchainAttestation(
+        attestationData,
+        signer
+      )
+      const isValid = await offchain.verifyOffchainAttestationSignature(
+        signer.address,
+        response
+      )
+
+      setIsValidSignature(isValid)
+    }
+
+    createAttestation()
+  }, [])
+
+  return (
+    <div>
+      <h1>Ethereum Attestation Service</h1>
+      <p>
+        The attestation signature is {isValidSignature ? 'valid' : 'invalid'}
+      </p>
+    </div>
+  )
+}
+
+export default AttestationComponent
