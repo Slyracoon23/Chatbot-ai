dffbb6674f9fc007b99c4c65463566d71e4c735c
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-23 01:30:27 +0200

    walletconnect

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
+
 import { IconGitHub, IconSeparator, IconVercel } from '@/components/ui/icons'
 import { ConnectButton } from './connect-button'
 
 export async function Header() {
+  // const session = await auth()
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
--- components/providers.tsx
+++ components/providers.tsx
@@ -25,9 +25,14 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
       </NextThemesProvider>
       <WalletConnectModalAuth
         projectId={'ce5ddda2d7ebe0e88dd74bc3b54a2cf7'}
+        modalOptions={{
+          explorerRecommendedWalletIds: [
+            'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96'
+          ]
+        }}
         metadata={{
           name: 'GraphID',
-          description: 'A Self-Sovereign Knowledge Graph',
+          description: 'Self-Sovereign Knowledge Graph',
           url: 'https://graphid.vercel.app',
           icons: ['https://graphid.vercel.app/logo.png']
         }}
