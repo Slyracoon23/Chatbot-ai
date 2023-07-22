9cbfd66493b4773c8b513af60fdb5fa0fbc7cb25
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-22 17:06:53 +0200

    integrate walletconnect auth

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,11 +5,23 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { WalletConnectModalAuth } from '@walletconnect/modal-auth-react'
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
-    </NextThemesProvider>
+    <>
+      <NextThemesProvider {...props}>
+        <TooltipProvider>{children}</TooltipProvider>
+      </NextThemesProvider>
+      <WalletConnectModalAuth
+        projectId={'ce5ddda2d7ebe0e88dd74bc3b54a2cf7'}
+        metadata={{
+          name: 'My Dapp',
+          description: 'My Dapp description',
+          url: 'https://my-dapp.com',
+          icons: ['https://my-dapp.com/logo.png']
+        }}
+      />
+    </>
   )
 }
