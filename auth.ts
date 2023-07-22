8e77e1aa27fc42dcb4c67a5fdc4d944bf630b3f2
Earl Potters <earl.potters@gmail.com>
2023-07-22 15:18:05 +0200

    add worldcoin auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import Auth02Provider from 'next-auth/providers/auth0'
 
 declare module 'next-auth' {
   interface Session {
@@ -15,7 +16,25 @@ export const {
   auth,
   CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    // GitHub,
+    Auth02Provider({
+      id: 'worldcoin',
+      name: 'worldcoin',
+      wellKnown: 'https://id.worldcoin.org/.well-known/openid-configuration',
+      authorization: "https://id.worldcoin.org/authorize",
+      issuer: 'https://id.worldcoin.org', 
+      clientId: 'app_ae12796fe25aa0e49f21304075b405a4',
+      clientSecret: 'sk_739db7c673a1732f7e29e8de22326d81364936d123891b88',
+      profile: profile => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email
+        }
+      }
+    })
+  ],
   callbacks: {
     jwt({ token, profile }) {
       if (profile) {
