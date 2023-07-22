d3787437015e083e8cb0bf70f75eb56d8af135c9
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-22 16:41:03 +0200

    chore: typecheck, format and lint entire project (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
-import Spotlight from '@/components/spotlight';
+import Spotlight from '@/components/spotlight'
 
 export const runtime = 'edge'
 
