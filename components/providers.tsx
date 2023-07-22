d4ce50c5966e26a4e8192de12eab5129435ad23c
Earl Potters <earl.potters@gmail.com>
2023-07-22 16:23:54 +0200

    Ep cypher intergration (#2)
    * add cypher query
    
    * add neo4j digital mirror
    
    * fix types errors on build
    
    ---------
    
    Co-authored-by: Otto G <5030059+ottodevs@users.noreply.github.com>

--- components/providers.tsx
+++ components/providers.tsx
@@ -7,6 +7,7 @@ import { ThemeProviderProps } from 'next-themes/dist/types'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { WalletConnectModalAuth } from '@walletconnect/modal-auth-react'
 
+
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <>
