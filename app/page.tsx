4b4a2f487295f533f82784b930655138609fd7d1
Lyons800 <oisinlyons13@gmail.com>
2023-07-23 04:36:41 +0200

    main page cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { ConnectButton } from '@/components/connect-button'
 import SocialModal from '@/components/social-modal'
 import { UserProfileCard } from '@/components/profile-card'
 import NotificationsCard from '@/components/notifications-card'
+import OtherProfile from '@/components/other-profile'
+import WalletInfo from '@/components/wallet-information'
 
 export const runtime = 'edge'
 
@@ -14,22 +16,6 @@ export default function IndexPage() {
   return (
     <div className="flex-row justify-around align-middle">
       <Spotlight />
-      <ConnectButton />
-      <div className="flex flex-col items-center justify-center gap-4">
-        <SocialModal
-          accountName="Account Name"
-          username="@therealkartik"
-          followers="10.2k"
-          following="599"
-        />
-
-        <UserProfileCard />
-
-        <NotificationsCard />
-
-      </div>
-
-
     </div>
   )
 }
