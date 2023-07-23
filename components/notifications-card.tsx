6ca5158d018ec44d7d2831b40f987b7527bef11b
Lyons800 <oisinlyons13@gmail.com>
2023-07-23 04:35:55 +0200

    notification card

--- components/notifications-card.tsx
+++ components/notifications-card.tsx
@@ -31,39 +31,34 @@ type CardProps = React.ComponentProps<typeof Card>
 
 export default function NotificationsCard({ className, ...props }: CardProps) {
   return (
-    <Card className={cn("bg-[rgba(255, 255, 0.04)] w-[380px] rounded-lg border border-[#2D2D2D]", className)} {...props}>
+    <Card className={cn("bg-[rgba(255, 255, 0.04)] w-[205px] rounded-lg border border-[#2D2D2D]", className)} {...props}>
       <CardHeader className="flex-row items-center justify-between p-3">
         <CardTitle className="text-sm font-medium text-gray-400">Notifications</CardTitle>
-        <Button className="text-[#ABFD2C] text-xs">
+        <Button className="h-2 w-16 bg-[#ABFD2C] text-xs text-[#171717]">
           Mark all as read
         </Button>
       </CardHeader>
-      <Separator className="w-full border border-[#2D2D2D]" />
+      <Separator className="w-full border-0 border-[#2D2D2D]" />
       <CardContent className="grid gap-4 p-5">
         <div className="space-y-4">
           {notifications.map((notification, index) => (
             <div
               key={index}
-              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+              className="mb-1 grid grid-cols-[25px_1fr] items-start pb-1 last:mb-0 last:pb-0"
             >
               <span className="flex h-2 w-2 translate-y-1 rounded-full bg-[#FFFFFF]" />
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none text-white">
+              <div className="space-y-0">
+                <p className="text-xs font-medium leading-none text-white">
                   {notification.title}
                 </p>
-                <p className="text-sm text-white">
+                {/* <p className="text-xs text-white">
                   {notification.description}
-                </p>
+                </p> */}
               </div>
             </div>
           ))}
         </div>
       </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <Check className="mr-2 h-4 w-4" /> Mark all as read
-        </Button>
-      </CardFooter>
     </Card>
   )
 }
